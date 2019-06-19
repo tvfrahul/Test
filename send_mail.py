7aaf7a1af167f13127874a4d3be0e8a4443b2279
tvfrahul <tvfrahul@gmail.com>
2019-06-19 21:20:33 +0530

    test

---
+++ send_mail.py
@@ -0,0 +1,35 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+def send_email(filename):
+    email_user = 'tvfrahul@gmail.com'
+    email_password = 'rahul.linkinpark@007'
+    email_send = 'tvfrahul@gmail.com'
+
+    subject = 'subject'
+
+    msg = MIMEMultipart()
+    msg['From'] = email_user
+    msg['To'] = email_send
+    msg['Subject'] = subject
+
+    body = 'Hi There, sending this modified from Python!'
+    msg.attach(MIMEText(body,'plain'))
+
+    attachment  =open(filename,'rb')
+
+    part = MIMEBase('application','octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+    msg.attach(part)
+    text = msg.as_string()
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login(email_user,email_password)
+    server.sendmail(email_user,email_send,text)
+    server.quit()
